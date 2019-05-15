29704455f0ad937aca06cd5932d3a985698fd1d2
Luke Phyall <shoreditchwarlock@gmail.com>
2019-05-15 16:39:35 +0100

    Adds index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Temperature extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>You're off</h1>
+        <h2>May God have mercy on your soul</h2>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
+  <Temperature />,
   document.getElementById('root')
 );
