422cc6ec1c31697b64a134aff46470c23987dfe7
Luke Phyall <shoreditchwarlock@gmail.com>
2019-05-17 11:27:02 +0100

    Adds local temperature to screen

--- src/index.js
+++ src/index.js
@@ -104,28 +104,34 @@ class Weather extends React.Component {
   constructor(props) {
     super(props);
 
-    const localTemp = () => {
-      fetch('https://api.openweathermap.org/data/2.5/weather?id=2636101&appid=86367bd791a2045eb9fff221d690c0af&units=metric')
-      .then(response => response.json())
-      .then((data) => {
-        const tenterdenTemp = data.main.temp;
-        console.log(tenterdenTemp);
-        return tenterdenTemp;
-      });
-    };
-
     this.state = {
       weather: 'What\'s the frequency, Kenneth',
       dummy: 'Dummy temp',
-      localTemp: localTemp(),
+      localTemp: 0,
     };
+
+    const getTemp = () => {
+      fetch('https://api.openweathermap.org/data/2.5/weather?id=2636101&appid=86367bd791a2045eb9fff221d690c0af&units=metric')
+      .then(response => response.json())
+      .then((data) => {
+
+        this.setState({
+          localTemp: data.main.temp,
+        });
+
+        const tenterdenTemp = data.main.temp;
+        console.log(tenterdenTemp);
+      });
+    };
+
+    getTemp();
   }
 
   render() {
     return (
       <div>
         <center>
-          <h3>{'Your local temperature is ' + this.state.localTemp}</h3>
+          <h3>{'Your local temperature is ' + this.state.localTemp + ' Celcius'}</h3>
         </center>
       </div>
     )
